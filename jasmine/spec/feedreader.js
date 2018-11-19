8c9953e21979c618d12de9442801c2be8ce6fabd
AhmedElaguab <elaguabahmed97@gmail.com>
2018-11-20 00:42:25 +0100

    Write a new test suite named 'New Feed Selection'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -120,11 +120,14 @@ $(function() {
         });
 
         /* TODO: Write a new test suite named "New Feed Selection" */
+        describe('New Feed Selection', function () {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+            /* TODO: Write a test that ensures when a new feed is loaded
+            * by the loadFeed function that the content actually changes.
+            * Remember, loadFeed() is asynchronous.
+            */
+
+        });
 
     });
 }());
