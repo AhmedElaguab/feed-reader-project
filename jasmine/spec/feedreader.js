c894d0df30b6f52898b3ae35ea0f3d09bf6f0226
AhmedElaguab <elaguabahmed97@gmail.com>
2018-11-19 17:23:04 +0100

    Write a new test suite named 'Initial Entries'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -101,6 +101,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
     /* TODO: Write a test that ensures when the loadFeed
      * function is called and completes its work, there is at least
@@ -115,4 +116,6 @@ $(function() {
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
      */
+
+     });
 }());
